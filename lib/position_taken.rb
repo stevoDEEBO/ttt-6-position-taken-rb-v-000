d0106babd88bd18693175f0134064463878bc060
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-02-22 15:48:36 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == " "
+  if board[index] == " " || board[index] == "" || board[index] == nil
     false
-  #elsif board[index] == 
+  #elsif board[index] ==
   end
 end
